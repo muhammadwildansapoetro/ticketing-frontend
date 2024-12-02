3c1b07c81fbb96517e80dbdbf4efd08530d8da28
muhammadwildansapoetro <muhwilsap@gmail.com>
2024-12-02 09:00:52 +0700

    add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/components/landing_page/hero";
 import MatchList from "@/components/landing_page/matchList";
 
 export default function Home() {
   return (
     <div>
+      <Hero />
       <MatchList />
     </div>
   );
