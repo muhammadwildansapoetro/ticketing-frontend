4585e79882f0a52e46b5d3c41620e73fa93b108d
muhammadwildansapoetro <muhwilsap@gmail.com>
2024-12-23 09:43:46 +0700

    update

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL_BE;
+const baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL_BE || "http://localhost:8000/api";
 
 export default axios.create({
   baseURL: baseURL,
